2735d0020bf9b85b3151e88697e77c95d60d7391
subhehmoden <hmed2sbhe@gmail.com>
2025-07-15 01:45:18 +0300

    fix app and set port

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
 import "react-toastify/dist/ReactToastify.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/flaticon.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/flaticon.css";
 import "../styles/font-awesome.min.css";
 import "../styles/themify-icons.css";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import 'photoswipe/dist/photoswipe.css'
-import '../styles/sass/style.scss'
+// import "photoswipe/dist/photoswipe.css";
+import "../styles/sass/style.scss";
 import Head from "next/head";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 function MyApp({ Component, pageProps }) {
@@ -26,8 +26,7 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
       <ToastContainer />
     </div>
-
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
